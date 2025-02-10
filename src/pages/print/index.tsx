e60158d715ea445331333d97ad2d97378c41b699
rowaxl <rowaxl0@gmail.com>
2025-02-10 14:27:24 +0900

    update: 複数ページ印刷対応

--- src/pages/print/index.tsx
+++ src/pages/print/index.tsx
@@ -15,8 +15,8 @@ export default function Page() {
   }, [contentRef])
 
   return (
-    <main className="print-page">
-      <div className="print-content" ref={contentRef}>
+    <main className="print-page" ref={contentRef}>
+      <div className="print-content page-break">
         <h1>Sample Letter Size Print</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
@@ -29,35 +29,97 @@ export default function Page() {
             </tr>
           </thead>
           <tbody>
-            <tr style={{ height: "30px" }}>
+            <tr>
               <td>Row 1</td>
               <td>Row 1</td>
               <td>Row 1</td>  
             </tr>
-            <tr style={{ height: "30px" }}>
+            <tr>
               <td>Row 2</td>
               <td>Row 2</td>
               <td>Row 2</td>
             </tr>
-            <tr style={{ height: "30px" }}>
+            <tr>
               <td>Row 3</td>
               <td>Row 3</td>
               <td>Row 3</td>
             </tr>
-            <tr style={{ height: "30px" }}>
+            <tr>
               <td>Row 4</td>
               <td>Row 4</td>
               <td>Row 4</td>
             </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="print-content page-break">
+        <h1>Sample Letter Size Print</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+        <table style={{ width: "100%", borderCollapse: "collapse" }}>
+          <thead>
             <tr style={{ height: "30px" }}>
-              <td>Row 5</td>
-              <td>Row 5</td>
-              <td>Row 5</td>
+              <th style={{ textAlign: "left" }}>Column 1</th>
+              <th style={{ textAlign: "left" }}>Column 2</th>
+              <th style={{ textAlign: "left" }}>Column 3</th>
             </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Row 1</td>
+              <td>Row 1</td>
+              <td>Row 1</td>  
+            </tr>
+            <tr>
+              <td>Row 2</td>
+              <td>Row 2</td>
+              <td>Row 2</td>
+            </tr>
+            <tr>
+              <td>Row 3</td>
+              <td>Row 3</td>
+              <td>Row 3</td>
+            </tr>
+            <tr>
+              <td>Row 4</td>
+              <td>Row 4</td>
+              <td>Row 4</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="print-content page-break">
+        <h1>Sample Letter Size Print</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+        <table style={{ width: "100%", borderCollapse: "collapse" }}>
+          <thead>
             <tr style={{ height: "30px" }}>
-              <td>Row 6</td>
-              <td>Row 6</td>
-              <td>Row 6</td>
+              <th style={{ textAlign: "left" }}>Column 1</th>
+              <th style={{ textAlign: "left" }}>Column 2</th>
+              <th style={{ textAlign: "left" }}>Column 3</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Row 1</td>
+              <td>Row 1</td>
+              <td>Row 1</td>  
+            </tr>
+            <tr>
+              <td>Row 2</td>
+              <td>Row 2</td>
+              <td>Row 2</td>
+            </tr>
+            <tr>
+              <td>Row 3</td>
+              <td>Row 3</td>
+              <td>Row 3</td>
+            </tr>
+            <tr>
+              <td>Row 4</td>
+              <td>Row 4</td>
+              <td>Row 4</td>
             </tr>
           </tbody>
         </table>
